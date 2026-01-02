adfaeb4d84678a266d85d1c90ea91dddaf516e3f
rotweb <rotweb_off@mail.ru>
2026-01-02 20:10:01 +0300

    create header

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontSize: {
+        standart_a: 'clamp(12px, 3vw, 18px)',
+        standart_but: '50px',
+      },
+    },
+  },
+}
