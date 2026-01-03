9a095961cdac1a351831d7cfbf2478fb8044e368
rotweb <rotweb_off@mail.ru>
2026-01-03 20:58:27 +0300

    add section2

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   theme: {
     extend: {
       fontSize: {
-        standart_a: 'clamp(12px, 3vw, 18px)',
+        standart_a: 'clamp(12px, 3vw, 24px)',
         standart_but: '50px',
       },
       fontFamily: {
