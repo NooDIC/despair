e30d637ff9a32a276b85f7b205b33783779c62f8
rotweb <rotweb_off@mail.ru>
2026-01-03 17:40:34 +0300

    add section1, add conf fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
         standart_a: 'clamp(12px, 3vw, 18px)',
         standart_but: '50px',
       },
+      fontFamily: {
+        oddval: ["Oddval", "sans-serif"],
+      },
     },
   },
 }
